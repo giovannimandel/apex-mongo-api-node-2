28a64abba34a1a779c0a3cedaf7f0be49f824fc5
giovannimandel <82120114+giovannimandel@users.noreply.github.com>
2024-06-30 22:41:41 -0300

    Update cidade.js

--- api/cidade.js
+++ api/cidade.js
@@ -1,4 +1,8 @@
+const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
 
 const CidadeSchema = new mongoose.Schema({
   _id: Number,
@@ -11,40 +15,38 @@ const Cidade = mongoose.models.Cidade || mongoose.model('Cidade', CidadeSchema);
 
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
-
   return mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
 };
 
-module.exports = async (req, res) => {
+app.get('/api/cidade', async (req, res) => {
   await connectDB();
-
-  if (req.method === 'GET') {
-    try {
-      const cidades = await Cidade.find();
-      res.status(200).json(cidades);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
+  try {
+    const cidades = await Cidade.find();
+    res.status(200).json(cidades);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
+});
 
-  if (req.method === 'POST') {
-    const { _id, nome, estado, populacao } = req.body;
+app.post('/api/cidade', async (req, res) => {
+  await connectDB();
+  const { _id, nome, estado, populacao } = req.body;
+  const novaCidade = new Cidade({
+    _id,
+    nome,
+    estado,
+    populacao
+  });
 
-    const novaCidade = new Cidade({
-      _id,
-      nome,
-      estado,
-      populacao
-    });
-
-    try {
-      const cidadeSalva = await novaCidade.save();
-      res.status(201).json(cidadeSalva);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
+  try {
+    const cidadeSalva = await novaCidade.save();
+    res.status(201).json(cidadeSalva);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-};
+});
+
+module.exports = app;
